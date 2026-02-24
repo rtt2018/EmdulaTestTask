8f5508e77e997a1498f9148698dab6f7e9029956
Taras <r.taras.t.1@gmail.com>
2026-02-24 21:06:49 +0200

    feat: add head component without logic

--- src/components/Calendar/Calendar.jsx
+++ src/components/Calendar/Calendar.jsx
@@ -1,3 +1,12 @@
 export default function Calendar() {
-  return <div className="">Calendar works!</div>;
+  return (
+    <div className="flex flex-col items-center justify-start gap-8 border-t border-white/25 p-7 w-[700px]">
+      <div className="flex flex-col items-start justify-start gap-6 w-[344px]">
+        <h4>Select a Date & Time</h4>
+        <div>calendar</div>
+        <div>TimeZone</div>
+      </div>
+      <div>Cookie settings</div>
+    </div>
+  );
 }
--- src/components/Head/Head.jsx
+++ src/components/Head/Head.jsx
@@ -5,9 +5,59 @@ export default function Head() {
         <h3 className="font-semibold text-2xl leading-[1.25] text-[#f4f1eb]">
           Сonsultation
         </h3>
-        <div className="flex flex-row items-start justify-start gap-4">
-          <p>30 min</p>
-          <p>Web conferencing details provided upon confirmation.</p>
+        <div className="flex flex-col items-start justify-start gap-6">
+          <div className="flex flex-wrap items-start justify-start flex-row gap-4">
+            <div className="flex items-center justify-start gap-2">
+              <svg
+                className="fill-none stroke-2 stroke-white/50"
+                width="20"
+                height="20"
+              >
+                <use href="/icon-sprite.svg#clock"></use>
+              </svg>
+              <p className="font-semibold text-base leading-6 text-white/50">
+                30 min
+              </p>
+            </div>
+            <div className="flex items-center justify-start gap-2">
+              <svg
+                className="fill-none stroke-2 stroke-white/50"
+                width="20"
+                height="20"
+              >
+                <use href="/icon-sprite.svg#camera"></use>
+              </svg>
+              <p className="font-semibold text-base leading-6 text-white/50">
+                Web conferencing details provided upon confirmation.
+              </p>
+            </div>
+          </div>
+          <div className="flex flex-wrap items-start justify-start flex-row gap-4">
+            <div className="flex items-center justify-start gap-2">
+              <svg
+                className="fill-none stroke-2 stroke-white/50"
+                width="20"
+                height="20"
+              >
+                <use href="/icon-sprite.svg#calendar"></use>
+              </svg>
+              <p className="font-semibold text-base leading-6 text-white/50">
+                10:00 - 10:30, Thuesday, February 12, 2026
+              </p>
+            </div>
+            <div className="flex items-center justify-start gap-2">
+              <svg
+                className="fill-none stroke-2 stroke-white/50"
+                width="20"
+                height="20"
+              >
+                <use href="/icon-sprite.svg#earth-TimeZone"></use>
+              </svg>
+              <p className="font-semibold text-base leading-6 text-white/50">
+                European Time
+              </p>
+            </div>
+          </div>
         </div>
       </div>
     </div>
